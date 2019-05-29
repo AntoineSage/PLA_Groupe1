ce11343e7f0689c308b205235b0fa74086317558
Hatinguais <bastien.hatinguais@gmail.com>
2019-05-29 16:48:34 +0200

    Add Condition.java & Aut.java

--- groupe1.info3.pla.game/game.automaton/src/ricm3/interpreter/Aut.java
+++ groupe1.info3.pla.game/game.automaton/src/ricm3/interpreter/Aut.java
@@ -1,5 +1,26 @@
 package ricm3.interpreter;
 
+import java.util.Iterator;
+
 public class Aut {
 
+	State m_initialState;
+	State m_currentState;
+
+	public Aut(State init, State current) {
+		m_initialState = init;
+		m_currentState = current;
+	}
+
+	public void step() {
+		Transition t;
+		Iterator<Transition> iter = m_currentState.m_transitions.iterator();
+		while (iter.hasNext()) {
+			t = iter.next();
+			if (t.m_condition.eval())
+				t.m_action.exec();
+			m_currentState = t.m_nextState;
+		}
+
+	}
 }
