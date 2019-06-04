2a45aa2735fd2ba15c948dd80bc47b2376cec110
Hatinguais <bastien.hatinguais@gmail.com>
2019-06-04 16:58:23 +0200

    Entity update

--- groupe1.info3.pla.game/game.purgatoire/edu/ricm3/game/purgatoire/Entity.java
+++ groupe1.info3.pla.game/game.purgatoire/edu/ricm3/game/purgatoire/Entity.java
@@ -1,21 +1,11 @@
 package edu.ricm3.game.purgatoire;
 
-import java.awt.Color;
-import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
-
-import ricm3.interpreter.IAutomaton;
-
 public class Entity {
 	int m_HP, m_maxHP;
 	int m_DMG;
 	int m_karmaToGive;
 	Stunt m_heavenStunt, m_hellStunt, m_currentStunt;
-	IAutomaton m_automaton;
 	Level m_level;
-	Color m_c;
-	Rectangle m_bounds;
-	BufferedImage m_sprite;
 
 	Entity() {
 
@@ -30,10 +20,6 @@ public class Entity {
 		m_hellStunt = hellStunt;
 		m_currentStunt = currentStunt;
 		m_level = level;
-		m_c = m_currentStunt.m_c;
-		m_automaton = m_currentStunt.m_automaton;
-		m_bounds = m_currentStunt.m_bounds;
-		m_sprite = m_currentStunt.m_sprite;
 	}
 
 	void transfrom() {
