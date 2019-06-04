45dad568c6cb5af5279f21282db6413a755f5577
Claire <claire.velut@laposte.net>
2019-06-04 17:05:06 +0200

    Added Options file

---
+++ groupe1.info3.pla.game/game.purgatoire/edu/ricm3/game/purgatoire/Options.java
@@ -0,0 +1,11 @@
+package edu.ricm3.game.purgatoire;
+
+public class Options {
+	
+	static final int WIN_HEIGHT = 1024;
+	static final int WIN_WIDTH = 768;
+	static final int LVL_HEIGHT = 84;
+	static final int LVL_WIDTH = 48;
+	static final int NB_BLOCKS_WIN = 55;
+
+}
