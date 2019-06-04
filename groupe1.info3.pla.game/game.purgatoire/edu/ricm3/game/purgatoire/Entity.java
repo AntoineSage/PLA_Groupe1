787c0527c0cc323eddb0d428b303816705878e9a
Hatinguais <bastien.hatinguais@gmail.com>
2019-06-04 17:01:05 +0200

    Entity update 2

--- groupe1.info3.pla.game/game.purgatoire/edu/ricm3/game/purgatoire/Entity.java
+++ groupe1.info3.pla.game/game.purgatoire/edu/ricm3/game/purgatoire/Entity.java
@@ -1,18 +1,21 @@
 package edu.ricm3.game.purgatoire;
 
+import java.awt.Rectangle;
+
 public class Entity {
 	int m_HP, m_maxHP;
 	int m_DMG;
 	int m_karmaToGive;
 	Stunt m_heavenStunt, m_hellStunt, m_currentStunt;
 	Level m_level;
+	Rectangle m_bounds;
 
 	Entity() {
 
 	}
 
 	Entity(int HP, int maxHP, int DMG, int karmaToGive, Stunt heavenStunt, Stunt hellStunt, Stunt currentStunt,
-			Level level) {
+			Level level, Rectangle bounds) {
 		m_HP = HP;
 		m_maxHP = maxHP;
 		m_DMG = DMG;
@@ -20,6 +23,7 @@ public class Entity {
 		m_hellStunt = hellStunt;
 		m_currentStunt = currentStunt;
 		m_level = level;
+		m_bounds = bounds;
 	}
 
 	void transfrom() {
